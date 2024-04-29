36f186abbdc97346e71cf1c8106c825e499e878a
Emmanuel <zacmac807@gmail.com>
2024-04-29 10:06:49 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+// import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    //fetching data from the server using an Api
+    //Avoid using network requests
+    fetch("http://jsonplaceholder.typicode.com/todos/2")
+      //awaiting the response
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    // the empty array is put to say only run useEffect once the page loads
+  }, []);
+  // console.log("Use effect has run"),[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React useEffect</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+
+      <p>{count}</p>
     </div>
   );
 }
